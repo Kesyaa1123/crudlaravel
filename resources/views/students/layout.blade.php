9c59d04f041709cb7cacc85531cf4bab539312bb
Kesyaa1123 <kesyaafiana30@gmail.com>
2023-02-20 11:00:56 +0700

    first commit

---
+++ resources/views/students/layout.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Student Laravel 9 CRUD</title>    
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+</head>
+<body>
+    
+<div class="contrainer">
+    @yield('content')
+</div>
+
+</body>
+</html>
